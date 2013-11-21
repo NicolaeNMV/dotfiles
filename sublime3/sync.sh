3549be3d1ce11b6f1fae3f716cbb0094d3149747
Nicolae Namolovan <nna@zenexity.com>
2013-11-21 17:54:01 +0100

    Fix the sublime sync script

--- sublime3/sync.sh
+++ sublime3/sync.sh
@@ -2,5 +2,5 @@
 #
 # Sync my sublime 3 settings
 
-cp "~/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings" "User/Package Control.sublime-settings" 
-cp "~/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" "User/Preferences.sublime-settings" 
+cp ~/"Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings" "User/"
+cp ~/"Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" "User/"
