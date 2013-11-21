1a341771fd7a6703dd1d88322acbe4ba42bc0091
Nicolae Namolovan <nna@zenexity.com>
2013-11-21 17:45:32 +0100

    A script to sync sublime 3 settings

---
+++ sublime3/sync.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# Sync my sublime 3 settings
+
+cp "~/Library/Application Support/Sublime Text 3/Packages/User/Package Control.sublime-settings" "User/Package Control.sublime-settings" 
+cp "~/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings" "User/Preferences.sublime-settings" 
